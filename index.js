75edf707092fefd60d4b6e6adc3dedf1fb1d8ecf
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-12-04 23:33:09 +0530

    Add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const searchBox = document.querySelector(".search_box");
 
-document.querySelector(".bx-search").onClick = () =>{
+document.querySelector(".bx-search").onClick = (e) =>{
+    e.preventDefault();
     searchBox.classList.toggle('active');
 }
